1c22b488f35ff57af40f2f2078f5afaef5e4551c
M IJAZ HUSSNAIN <mijazhussnain83@gmail.com>
2025-11-13 02:06:54 +0500

    Commit

--- myapp/todo/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Task
-
-# Register your models here.
-@admin.register(Task)
-class TaskAdmin(admin.ModelAdmin):
-    list_display = ('title', 'status', 'priority', 'created_at', 'due_date')
-    list_filter = ('status', 'priority')
-    search_fields = ('title',)
